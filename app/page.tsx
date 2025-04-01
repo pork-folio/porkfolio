88eeb30462b0ee08b5192887085f162ca8835329
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-04-01 16:55:35 +0200

    Core assets (#4)

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,20 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { DataTable } from "@/components/data-table";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import * as core from "@/core";
 
 import data from "./data.json";
 
-export default function Page() {
+export default async function Page() {
+  // showcase that it works
+  const assets = core.supportedAssets();
+
+  console.log("Supported assets", assets);
+
+  const prices = await core.queryAssetPrices(assets);
+
+  console.log("Pyth Prices", prices);
+
   return (
     <SidebarProvider>
       <AppSidebar variant="inset" />
