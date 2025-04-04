1663639bee9269a4471f484bee20fce742318c3b
Denis Fadeev <denis@fadeev.org>
2025-04-04 15:33:09 +0300

    fix image warnings

--- components/balances-table.tsx
+++ components/balances-table.tsx
@@ -739,6 +739,8 @@ export function BalancesTable({
                 className="w-full h-full object-cover object-top"
                 width={1200}
                 height={400}
+                style={{ width: "auto", height: "auto" }}
+                priority
               />
 
               {/* Smoothly fading blur */}
